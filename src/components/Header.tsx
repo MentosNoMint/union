f974e61080404620f2dc11f2dbb1a031f9594841
MentosNoMint <dima-krut-2020@bk.ru>
2025-03-12 15:08:07 +0500

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+        <header className={'w-full flex justify-between mt-7 px-[4.75rem]'}>
+            <span>Logo</span>
+            <div className={'flex gap-3 items-center'}>
+
+            </div>
+        </header>
+    )
+};
+
+export default Header;
